7f91f0b48823ae99d808142221b5f723eb8d3c85
fasugba boluwatife <fasugbab@gmail.com>
2019-07-10 15:48:09 +0100

    first commit

--- test.py
+++
@@ -1,7 +0,0 @@
-import csv
-with open('slcspcc.csv', 'w', newline='') as csvfile:
-    spamwriter = csv.writer(csvfile)
-    spamwriter.writerow(['Spam'] * 5 + ['Baked Beans'])
-    spamwriter.writerow(['Spam', 'Lovely Spam', 'Wonderful Spam'])
-
-
